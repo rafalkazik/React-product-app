587cd3931bccce46ef7690e35f17cf2c06fcb1b1
Rafał Kazik <kazik.raf@gmail.com>
2022-03-22 23:48:08 +0100

    login style

--- src/app/components/Login/Login.tsx
+++ src/app/components/Login/Login.tsx
@@ -1,29 +1,51 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import '../../styles/components/Login.scss';
+
 import { AppRoute } from 'routing/AppRoute.enum';
 
 export const Login = () => {
   return (
-    <div>
-      <h2>Login</h2>
-      <form>
-        <div>
-          <label>
-            username:
-            <input name='username' />
-          </label>
-        </div>
-        <div>
-          <label>
-            password:
-            <input name='password' type='password' />
-          </label>
-        </div>
-        <Link to={AppRoute.Home}>
-          <button type='submit'>submit</button>
-        </Link>
-      </form>
+    <div className='login__wrapper login'>
+      <div className='login__photo'></div>
+      <div className='login__content'>
+        <h2 className='login__logo'>join.tsh.io</h2>
+        <h2 className='login__title'>Login</h2>
+        <form className='login__login-form login-form'>
+          <div className='login-form__input-wrapper'>
+            <label className='login-form__label'>
+              Username:
+              <input
+                name='username'
+                className='login-form__input'
+                placeholder='Enter username'
+              />
+            </label>
+          </div>
+          <div className='login-form__input-wrapper'>
+            <label className='login-form__label'>
+              Password:
+              <input
+                name='password'
+                type='password'
+                className='login-form__input'
+                placeholder='Enter password'
+              />
+            </label>
+          </div>
+
+          <Link to={AppRoute.Home}>
+            <button type='submit' className='login-form__submit'>
+              Log in
+            </button>
+          </Link>
+
+          <a className='login-form__forgot-pass' href='#'>
+            Forgot password?
+          </a>
+        </form>
+      </div>
     </div>
   );
 };
