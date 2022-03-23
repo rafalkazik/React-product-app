0e126b7552714da22530da94eedfffd7dc08dfa0
Rafał Kazik <kazik.raf@gmail.com>
2022-03-23 18:44:11 +0100

    login style fix

--- src/app/components/Login/Login.tsx
+++ src/app/components/Login/Login.tsx
@@ -11,40 +11,42 @@ export const Login = () => {
       <div className='login__photo'></div>
       <div className='login__content'>
         <h2 className='login__logo'>join.tsh.io</h2>
-        <h2 className='login__title'>Login</h2>
-        <form className='login__login-form login-form'>
-          <div className='login-form__input-wrapper'>
-            <label className='login-form__label'>
-              Username:
-              <input
-                name='username'
-                className='login-form__input'
-                placeholder='Enter username'
-              />
-            </label>
-          </div>
-          <div className='login-form__input-wrapper'>
-            <label className='login-form__label'>
-              Password:
-              <input
-                name='password'
-                type='password'
-                className='login-form__input'
-                placeholder='Enter password'
-              />
-            </label>
-          </div>
+        <div className='login__login-form-wrapper'>
+          <h2 className='login__title'>Login</h2>
+          <form className='login__login-form login-form'>
+            <div className='login-form__input-wrapper'>
+              <label className='login-form__label'>
+                Username:
+                <input
+                  name='username'
+                  className='login-form__input'
+                  placeholder='Enter username'
+                />
+              </label>
+            </div>
+            <div className='login-form__input-wrapper'>
+              <label className='login-form__label'>
+                Password:
+                <input
+                  name='password'
+                  type='password'
+                  className='login-form__input'
+                  placeholder='Enter password'
+                />
+              </label>
+            </div>
 
-          <Link to={AppRoute.Home}>
-            <button type='submit' className='login-form__submit'>
-              Log in
-            </button>
-          </Link>
+            <Link to={AppRoute.Home}>
+              <button type='submit' className='login-form__submit'>
+                Log in
+              </button>
+            </Link>
 
-          <a className='login-form__forgot-pass' href='#'>
-            Forgot password?
-          </a>
-        </form>
+            <a className='login-form__forgot-pass' href='#'>
+              Forgot password?
+            </a>
+          </form>
+        </div>
       </div>
     </div>
   );
